9ff9a765970c6a6b9babd5930324875d15f0063f
AissaniOussama <aissani.ous@gmail.com>
2025-04-16 10:17:24 +0200

    1 Milestone 0.1

---
+++ src/main/java/de/htwberlin/webtech/Controller.java
@@ -0,0 +1,10 @@
+package de.htwberlin.webtech;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+public class Controller {
+
+
+
+
+}
+
---
+++ src/main/java/de/htwberlin/webtech/Stickman.java
@@ -0,0 +1,12 @@
+package de.htwberlin.webtech;
+
+public class Stickman {
+
+
+
+
+
+
+
+
+}
