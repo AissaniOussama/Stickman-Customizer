074bdd4a60c13adb8b90319717b3fb94fb3493b1
AissaniOussama <aissani.ous@gmail.com>
2025-05-21 10:57:28 +0200

    Milestone 3 Dockerfile added

--- src/main/java/de/htwberlin/webtech/Controller.java
+++ src/main/java/de/htwberlin/webtech/Controller.java
@@ -1,6 +1,8 @@
 package de.htwberlin.webtech;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -11,11 +13,15 @@ public class Controller {
     public List<Stickman> LogInDataRouteTest() {
         return List.of(
                 new Stickman("Stickman1", "SwagKeys for the Attributes"),
-                new Stickman("Stickman2","SwagKeys for the Attributes"),
-                new Stickman("Stickman3","SwagKeys for the Attributes")
+                new Stickman("Stickman2", "SwagKeys for the Attributes"),
+                new Stickman("Stickman3", "SwagKeys for the Attributes")
         );
     }
+
+
+    @GetMapping("/Stickmans")
+    public List<Stickman> getAllThings() {
+        return LogInDataRouteTest();
+
+    }
 }
-
-
-
